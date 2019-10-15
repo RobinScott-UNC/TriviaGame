820cc84d7c657ec8e7d31dc86d4d26cd3997ca43
Robin Scott <robinreyes31@hotmail.com>
2019-10-15 08:46:26 -0400

    "added more .js and JQuery for START countdown timer button"

--- Assets/javascript/TriviaGame.js
+++ Assets/javascript/TriviaGame.js
@@ -1,4 +1,38 @@
 
+$(function(){
+    myCounter.Init();
+});
+
+var myCounter = {
+    Init: function() {
+        this.$target = $( "#START").attr("disabled", true);
+        this.EnableSTART();
+        this.Eventhandler();
+    },
+
+    EnableSubmit: function() {
+        var value = this.Get("START-button");
+        var now = now();
+        if( !value ) {
+            this.$target.removeAttr("disabled");
+            return;
+        }
+        setTimeout(function() {
+            Program.EnableSubmit(); },
+            value - now;
+        },
+
+        Submit: function() {
+            this.$target.attr("disabled", true);
+            this.Set("START-button", Date.now() + 30000, {
+                expire_in: "seconds"
+            });
+
+        Eventhandler: function(){
+            this.$target.on("click", function() {
+                Program.Subit();
+        },
+
 var myCounter = new Countdown ( { 
     seconds: 30,
     onUpdateStatus: function(sec) {console.log(sec);},
@@ -9,7 +43,11 @@ myCounter.start();
 
 class Countdown {
     constructor(options) {
-        var timer, instance = this, seconds = options.seconds || 30, updateStatus = options.onUpdateStatus || function () { }, counterEnd = options.onCounterEnd || function () { };
+        var timer, instance = this;
+        seconds = options.seconds || 30; 
+        updateStatus = options.onUpdateStatus || function () { };
+        counterEnd = options.onCounterEnd || function () { };
+
         function decrementCounter() {
             updateStatus(seconds);
             if (seconds === 0) {
